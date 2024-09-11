f6251e93e4a4b53615fbe3c3ec3a54cbc214b940
Josh Lakenan <joshualakenan@gmail.com>
2024-09-11 07:38:27 -0500

    Creates and syncs a sequelize user model
    The user model is imported within app.js to ensure it is initialized /
    defined within sequelize instance before  running the sync command,
    which creates the table in the postgres database if it does not yet
    exist.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,14 @@
 import express from "express";
 import sequelize from "./models/index.js";
+import User from "./models/user.js";
+
+try {
+  // Synchronize the models with the database
+  await sequelize.sync({ force: false });
+  console.log("All models were synchronized successfully.");
+} catch (error) {
+  throw new Error("Unable to synchronize the models with the database:", error);
+}
 
 const app = express();
 
